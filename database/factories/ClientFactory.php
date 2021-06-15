cf3ea30207683323581ee2f8a8939060097a10e3
Vinícius Meira <viniciusmeirabr@hotmail.com>
2021-06-15 08:31:19 -0300

    Ajustes na factory de Clientes

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -26,7 +26,7 @@ class ClientFactory extends Factory
             'name' => $this->faker->name,
             'cpf' => substr(str_shuffle("0123456789"), 0, 12),
             'rg' => substr(str_shuffle("0123456789"), 0, 11),
-            'birth_date' => $this->faker->dateTimeBetween('-60 years','-2 years'),
+            'birth_date' => $this->faker->dateTimeBetween('-60 years','-18 years'),
             'phone' => substr(str_shuffle("0123456789"), 0, 12),
             'state' =>  $this->faker->randomElement(['BA', 'SP'])
         ];
